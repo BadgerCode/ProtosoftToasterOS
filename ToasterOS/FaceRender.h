bc5a0ad7b87a77f4085b1e99e871616e88565f13
Michael Hawkins <BadgerCode@users.noreply.github.com>
2025-09-02 20:00:01 +0100

    Re-add grouping optimisation for panels

--- ToasterOS/FaceRender.h
+++ ToasterOS/FaceRender.h
@@ -12,11 +12,25 @@ private:
   const int Brightness = 15;  // 0 - 15
   byte EmptyPanel[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
 
+  // Face LED state
+  // 2 sides with 7 panels, 8 rows per panel
+  byte FaceLEDRowValues[TOTAL_LED_PANELS][8];
+  bool FaceLEDRowRequiresRendering[TOTAL_LED_PANELS][8];
+
   // LED interface
   int NumLEDControls;
   LedControl2** LEDControls;
 
   FacePanelConfig PanelConfigs[TOTAL_LED_PANELS];
+
+  // Rendering
+  unsigned int NextRenderSection = 0;  // 0 = eyes, 1 = nose, 2 = mouth
+
+  // Split the face into sections, so we can render all panels for a section at the same time
+  int EyePanelTypes[4] = { PANEL_LEFT_EYE_BACK, PANEL_LEFT_EYE_FRONT, PANEL_RIGHT_EYE_BACK, PANEL_RIGHT_EYE_FRONT };
+  int NosePanelTypes[2] = { PANEL_LEFT_NOSE, PANEL_RIGHT_NOSE };
+  int MouthPanelTypes[8] = { PANEL_LEFT_MOUTH_BACK, PANEL_LEFT_MOUTH_MID_BACK, PANEL_LEFT_MOUTH_MID_FRONT, PANEL_LEFT_MOUTH_FRONT,
+                             PANEL_RIGHT_MOUTH_BACK, PANEL_RIGHT_MOUTH_MID_BACK, PANEL_RIGHT_MOUTH_MID_FRONT, PANEL_RIGHT_MOUTH_FRONT };
 public:
   FaceRender(FaceConfig* faceConfig) {
     NumLEDControls = faceConfig->NumConnections;
@@ -80,12 +94,71 @@ public:
         rowData = flipX ? Reverse(data[rowDataIndex]) : data[rowDataIndex];
       }
 
-      panelConfig.Controller->SetRow(panelConfig.Address, row, rowData);
+      UpdatePanelRow(panelType, row, rowData);
     }
   }
 
+  void UpdatePanelRow(int panelType, int row, byte output) {
+    // If the output hasn't changed, do nothing
+    byte previousOutput = FaceLEDRowValues[panelType][row];
+    if (previousOutput == output) return;
+
+    // Update LED output to the new output
+    FaceLEDRowValues[panelType][row] = output;
+    FaceLEDRowRequiresRendering[panelType][row] = true;
+
+    // Rendering is handled by the render queue- processRenderQueue()
+  }
+
+
   // Render face
   void ProcessRenderQueue() {
+    bool hasRenderedASection = false;
+
+    // Check if any sections need rendering (eyes, nose, mouth)
+    for (int i = 0; i < 3; i++) {
+      // Only render 1 section at a time
+      if (hasRenderedASection) break;
+
+      int* panelTypes;
+      int numPanelTypes;
+
+      // Determine the panels, based on the section
+      if (NextRenderSection == 0) {  // eyes
+        panelTypes = EyePanelTypes;
+        numPanelTypes = 4;
+      } else if (NextRenderSection == 1) {  // nose
+        panelTypes = NosePanelTypes;
+        numPanelTypes = 2;
+      } else if (NextRenderSection == 2) {  // mouth
+        panelTypes = MouthPanelTypes;
+        numPanelTypes = 8;
+      } else {  // something's gone wrong
+        NextRenderSection = 0;
+        break;
+      }
+
+
+      // Render all panels with updates for the section
+      for (int p = 0; p < numPanelTypes; p++) {
+        int panelType = panelTypes[p];
+        auto panelConfig = PanelConfigs[panelType];
+
+        for (int row = 0; row < 8; row++) {
+          if (!FaceLEDRowRequiresRendering[panelType][row]) continue;
+
+          panelConfig.Controller->SetRow(panelConfig.Address, row, FaceLEDRowValues[panelType][row]);
+
+          FaceLEDRowRequiresRendering[panelType][row] = false;
+          hasRenderedASection = true;
+        }
+      }
+
+      // Move onto the next section
+      NextRenderSection++;
+      if (NextRenderSection > 2) NextRenderSection = 0;
+    }
+
     for (int c = 0; c < NumLEDControls; c++) {
       auto controller = LEDControls[c];
       controller->RenderDisplays();
