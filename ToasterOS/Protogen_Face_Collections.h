a608cc64295b6a1a4669c8d29f33b956810e817d
Michael Hawkins <BadgerCode@users.noreply.github.com>
2025-12-12 02:23:00 +0000

    Remove unused random expressions

--- ToasterOS/Protogen_Face_Collections.h
+++ ToasterOS/Protogen_Face_Collections.h
@@ -1,16 +1,4 @@
 // Face collections
-const int NumRandomExpressions = 7;
-struct FaceExpression* RandomExpressions[NumRandomExpressions] = {
-  &Face_Happy_Closed_eyes,
-  &Face_OWO,
-  &Face_Smirk,
-  &Face_Cute,
-  &Face_Blep,
-  &Face_Spiral,
-  &Face_Silly
-};
-
-
 const int NumHappyExpressions = 4;
 struct FaceExpression* HappyExpressions[NumHappyExpressions] = {
   &Face_Happy_Closed_eyes,
